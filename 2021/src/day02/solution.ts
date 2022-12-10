44c6a3963d9e76fd6c8a6d3e423490275b406f42
Deyan Varchev <varchev@officernd.com>
2022-12-10 21:42:34 +0200

    formatting

--- 2021/src/day02/solution.ts
+++ 2021/src/day02/solution.ts
@@ -12,23 +12,27 @@ forward 2`,
 };
 
 function parseInput(rawInput: string): [string, number][] {
-  return rawInput.split('\n').map(l => l.split(' ')).map(([cmd, n]) => [cmd, +n]);
+  return rawInput
+    .split("\n")
+    .map((l) => l.split(" "))
+    .map(([cmd, n]) => [cmd, +n]);
 }
 
-const moves : { [key: string]: [number,number] } = {
+const moves: { [key: string]: [number, number] } = {
   up: [0, -1],
   down: [0, 1],
-  forward: [1, 0]
-}
+  forward: [1, 0],
+};
 
 export function solvePart1(rawInput: string) {
   const input = parseInput(rawInput);
-  let pos = [0,0];
-  input.forEach(([cmd, n]) =>{
-    let [x,y] = pos, move = moves[cmd];
+  let pos = [0, 0];
+  input.forEach(([cmd, n]) => {
+    let [x, y] = pos,
+      move = moves[cmd];
     x += move[0] * n;
     y += move[1] * n;
-    pos = [x,y];
+    pos = [x, y];
   });
 
   return pos[0] * pos[1];
@@ -41,13 +45,14 @@ const t2 = {
 
 export function solvePart2(rawInput: string) {
   const input = parseInput(rawInput);
-  let pos = [0,0,0];
-  input.forEach(([cmd, n]) =>{
-    let [x,y,a] = pos, move = moves[cmd];
+  let pos = [0, 0, 0];
+  input.forEach(([cmd, n]) => {
+    let [x, y, a] = pos,
+      move = moves[cmd];
     x += move[0] * n;
     a += move[1] * n;
     y += move[0] * a * n;
-    pos = [x,y,a];
+    pos = [x, y, a];
   });
 
   return pos[0] * pos[1];
